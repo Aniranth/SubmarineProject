8169b27839a4d56e2aa99869be38758544e90f86
ArclingML <arcling@Han-Tyumi.localdomain>
2021-04-26 17:16:33 -0700

    working on cutscene

---
+++ Assets/Scripts/TextScroll.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScroll : MonoBehaviour
+{
+
+    public string text;
+    public Text box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TypeSentence(text));    
+    }
+
+    IEnumerator TypeSentence(string text){
+        box.text = "";
+        foreach(char c in text.ToCharArray()){
+            box.text += c;
+            yield return null;
+        }
+    }
+
+}
